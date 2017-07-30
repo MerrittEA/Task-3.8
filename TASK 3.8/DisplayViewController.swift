689f55051f2ef5ada29a794b51cca453ddf7a15e
Merritt Adkins <MerrittE@protonmail.com>
2017-07-29 22:32:15 -1000

    Trying to figure out how to access the saved data and display it on the split view controller. I think I need to add something about which index I am selecting for this to work right.

--- TASK 3.8/DisplayViewController.swift
+++ TASK 3.8/DisplayViewController.swift
@@ -10,40 +10,83 @@ import UIKit
 
 class DisplayViewController: UIViewController {
     
-    var choresForDisplay = [ChoreCategory]()
+    var displayChores:  ChoreCategory?
+    var chores = [ChoreCategory]()
+    
     
     @IBOutlet weak var choreNameDisplay: UILabel!
-    @IBOutlet weak var choreRewardDisplay: UISegmentedControl!
+    @IBOutlet weak var choreRewardDisplay: UILabel!
     @IBOutlet weak var starImageDisplay: UIImageView!
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
-    
-        if let choresForDisplay = SaveDataModel.sharedInformation.loadChores() {
-            choreNameDisplay.text = choresForDisplay.name
-            
-        }
-       
         
+        
+        let fileManager = FileManager.default
+        let databaseFileName = "swifty.sqlite"
+        var databaseUrl: URL? = nil
+        
+        do {
+            let baseUrl = try
+                fileManager.url(for: FileManager.SearchPathDirectory.documentDirectory, in: FileManager.SearchPathDomainMask.userDomainMask, appropriateFor: nil, create: false)
+            databaseUrl = baseUrl.appendingPathComponent(databaseFileName)
+        } catch {
+            print(error)
+        }
+        
+        if let databaseUrl = databaseUrl {
+            let fmdb = FMDatabase(path: databaseUrl.absoluteString)
+            fmdb.open()
+        
+        let selectSql = "select * from ChoreTable"
+            if let fmbdResult = fmdb.executeQuery(selectSql, withParameterDictionary: nil) {
+                chores.removeAll()
+            while (fmbdResult.next()) {
+                let rowId = fmbdResult.int(forColumn: SaveDataModel.idFieldName)
+                let rewardCount = fmbdResult.string(forColumn: ChoreCategory.PropertyNames.rewardCountField)
+                let choreName = fmbdResult.string(forColumn: ChoreCategory.PropertyNames.choreNameField) ?? ""
+                let starType = fmbdResult.string(forColumn: ChoreCategory.PropertyNames.starTypeField) ?? ""
+                    
+                choreNameDisplay.text = choreName
+                choreRewardDisplay.text = rewardCount
+                starImageDisplay.image = UIImage(named: starType)
+                }
+            }
+        }
+    }
+        
+    
+        //choresForDisplay = SaveDataModel.sharedInformation.loadChores()
+        //let name = ChoreCategory.PropertyNames.choreNameField
+        //let reward = ChoreCategory.PropertyNames.rewardCountField
+        //let star = ChoreCategory.PropertyNames.starTypeField
+        
+        //choreNameDisplay.text = ""
+        //choreRewardDisplay.text = reward
+        //starImageDisplay.image = UIImage(named: star)
+            //choresForDisplay.choreName = choreNameDisplay.text
+            //choresForDisplay.rewardCount = choreRewardDisplay.text
+            //choresForDisplay.starType = starImageDisplay.image
+        //}
+    
+        
+        //if let choresItem = self.displayChores {
+           // navigationItem.title = choresItem.choreName
+           // choreRewardDisplay.text = ChoreCategory.PropertyNames.rewardCountField
+            //starImageDisplay.image = UIImage(named:choresItem.starType)
+        //}
+            
+    //}
+
+
 
         // Do any additional setup after loading the view.
-    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
