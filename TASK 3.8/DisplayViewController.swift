4dc76d2199931430265fe2679087d962f5ea01bb
Merritt Adkins <MerrittE@protonmail.com>
2017-08-05 14:28:33 -1000

    Trying to fix the saving issues by adding ID call

--- TASK 3.8/DisplayViewController.swift
+++ TASK 3.8/DisplayViewController.swift
@@ -13,7 +13,7 @@ class DisplayViewController: UIViewController {
     var displayChores:  ChoreCategory?
     var chores = [ChoreCategory]()
     
-    
+    var choreID = Int()
     @IBOutlet weak var choreNameDisplay: UILabel!
     @IBOutlet weak var choreRewardDisplay: UILabel!
     @IBOutlet weak var starImageDisplay: UIImageView!
@@ -22,62 +22,7 @@ class DisplayViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         reFreshUI()
-        
-        //let fileManager = FileManager.default
-        //var databaseUrl: URL? = nil
-        
-        //do {
-            //let baseUrl = try
-                //fileManager.url(for: FileManager.SearchPathDirectory.documentDirectory, in: FileManager.SearchPathDomainMask.userDomainMask, appropriateFor: nil, create: false)
-            //databaseUrl = baseUrl.appendingPathComponent("swifty.sqlite")
-        //}catch {
-            //print(error)
-        //}
-
-        //if let databaseUrl = databaseUrl {
-            //let fmdb = FMDatabase(path: databaseUrl.absoluteString)
-            //fmdb.open()
-        
-        //let selectSql = "select * from ChoreTable"
-            //if let fmbdResult = fmdb.executeQuery(selectSql, withParameterDictionary: nil) {
-               //chores.removeAll()
-            //while (fmbdResult.next()) {
-                //let rowId = fmbdResult.int(forColumn: SaveDataModel.idFieldName)
-                //let rewardCount = fmbdResult.string(forColumn: ChoreCategory.PropertyNames.rewardCountField)
-                //let choreName = fmbdResult.string(forColumn: ChoreCategory.PropertyNames.choreNameField) ?? ""
-                //let starType = fmbdResult.string(forColumn: ChoreCategory.PropertyNames.starTypeField) ?? ""
-                
-                //choreNameDisplay.text = choreName
-                //choreRewardDisplay.text = rewardCount
-                //starImageDisplay.image = UIImage(named: starType)
-                //reFreshUI()
-                //}
-            //}
-            //fmdb.close()
-        //}
     }
-        
-    
-        //choresForDisplay = SaveDataModel.sharedInformation.loadChores()
-        //let name = ChoreCategory.PropertyNames.choreNameField
-        //let reward = ChoreCategory.PropertyNames.rewardCountField
-        //let star = ChoreCategory.PropertyNames.starTypeField
-        
-        //choreNameDisplay.text = ""
-        //choreRewardDisplay.text = reward
-        //starImageDisplay.image = UIImage(named: star)
-            //choresForDisplay.choreName = choreNameDisplay.text
-            //choresForDisplay.rewardCount = choreRewardDisplay.text
-            //choresForDisplay.starType = starImageDisplay.image
-        //}
-    
-        
-        //if let choresItem = self.displayChores {
-            //choreNameDisplay.text = choresItem.choreName
-            //choreRewardDisplay.text = choresItem.rewardCount
-            //starImageDisplay.image = UIImage(named: choresItem.starType!)
-        //}
-    //}
 
 
 
@@ -105,13 +50,19 @@ class DisplayViewController: UIViewController {
         let fmdb = FMDatabase(path: databaseUrl.absoluteString)
         fmdb.open()
         
-        let selectSql = "select * from ChoreTable"
-        if let fmbdResult = fmdb.executeQuery(selectSql, withParameterDictionary: nil) {
+        
+        let selectSql = "select * from ChoreTable WHERE ID = ?"
+        if let fmdbResult = fmdb.executeQuery(selectSql, withParameterDictionary: nil) {
             chores.removeAll()
-            while (fmbdResult.next()) {
-                choreNameDisplay.text =  fmbdResult.string(forColumn: ChoreCategory.PropertyNames.choreNameField) ?? ""
-                choreRewardDisplay.text = fmbdResult.string(forColumn: ChoreCategory.PropertyNames.rewardCountField)
-                starImageDisplay.image =  UIImage(named: fmbdResult.string(forColumn: ChoreCategory.PropertyNames.starTypeField) ?? "")
+            while (fmdbResult.next()) {
+                let rowID = fmdbResult.int(forColumn: "ID")
+                let choreName =  fmdbResult.string(forColumn: ChoreCategory.PropertyNames.choreNameField) ?? ""
+                let choreReward = fmdbResult.string(forColumn: ChoreCategory.PropertyNames.rewardCountField)
+                let starImage =  UIImage(named: fmdbResult.string(forColumn: ChoreCategory.PropertyNames.starTypeField) ?? "")
+                
+                choreNameDisplay.text = choreName
+                choreRewardDisplay.text = choreReward
+                starImageDisplay.image = starImage
             }
         }
             fmdb.close()
